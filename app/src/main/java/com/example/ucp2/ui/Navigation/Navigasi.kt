8fb846ecc9e1836d4f3b3ab7a5ca4038fcdc7a90
cahyadie <cahyadiilham12@gmail.com>
2024-12-19 16:28:03 +0700

    Menambahkan Navigasi

---
+++ app/src/main/java/com/example/ucp2/ui/Navigation/Navigasi.kt
@@ -0,0 +1,22 @@
+package com.example.ucp2.ui.Navigation
+
+interface Navigasi {
+    val route: String
+
+    object DestinasiHome : Navigasi {
+        override val route = "home"
+    }
+
+    object DestinasiDetail : Navigasi {
+        override val route = "detail"
+        const val Kode = "kode"
+        val routeWithArg = "$route/{$Kode}"
+    }
+
+    object DestinasiUpdate : Navigasi {
+        override val route = "update"
+        const val Nidn = "nidn"
+        const val Kode = "kode"
+        val routeWithArg = "$route/{$Nidn}/{$Kode}"
+    }
+}
