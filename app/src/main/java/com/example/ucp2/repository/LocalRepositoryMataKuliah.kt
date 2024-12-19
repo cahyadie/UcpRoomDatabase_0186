41b1ccd13ffc83a1cfa4a9fa14a90f16bc7fa5d6
cahyadie <cahyadiilham12@gmail.com>
2024-12-19 16:12:25 +0700

    mengubah localrepository menjadi 2

--- app/src/main/java/com/example/ucp2/repository/LocalRepositoryMataKuliah.kt
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryMataKuliah.kt
@@ -1,31 +1,15 @@
 package com.example.praktikum9.repository
 
-import com.example.ucp2.Data.dao.DosenDao
 import com.example.ucp2.Data.dao.MataKuliahDao
-import com.example.ucp2.Data.entity.Dosen
 import com.example.ucp2.Data.entity.MataKuliah
 import com.example.ucp2.repository.RepositoryMataKuliah
 
 import kotlinx.coroutines.flow.Flow
 
-class LocalRepository(
-    private val dosenDao: DosenDao,
+class LocalRepositoryMataKuliah(
     private val mataKuliahDao: MataKuliahDao
 
-) : RepositoryDosen,
-    RepositoryMataKuliah {
-    override suspend fun insertDosen(dosen: Dosen) {
-        dosenDao.insertDosen(dosen)
-    }
-
-    override fun getAllDosen(): Flow<List<Dosen>> {
-        return dosenDao.getAllDosen()
-    }
-
-    override fun getDosen(nidn: String): Flow<Dosen> {
-        return dosenDao.getDosen(nidn)
-    }
-
+) : RepositoryMataKuliah {
     override suspend fun insertMataKuliah(mataKuliah: MataKuliah)  {
         mataKuliahDao.insertMataKuliah(mataKuliah)
     }
