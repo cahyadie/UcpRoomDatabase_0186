dc28a2044b7444d79de03cd9944bcc5e059d5e88
cahyadie <cahyadiilham12@gmail.com>
2024-12-19 16:13:47 +0700

    menambahkkan containerApp

--- app/src/main/java/com/example/ucp2/Depedenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2/Depedenciesinjection/ContainerApp.kt
@@ -1,1 +1,25 @@
 package com.example.praktikum9.Depedenciesinjection
+
+import android.content.Context
+import com.example.praktikum9.repository.LocalRepositoryDosen
+import com.example.praktikum9.repository.LocalRepositoryMataKuliah
+import com.example.praktikum9.repository.RepositoryDosen
+import com.example.ucp2.Data.database.DatabaseAll
+import com.example.ucp2.repository.RepositoryMataKuliah
+
+interface InterfaceContainerApp{
+    val repositoryDosen: RepositoryDosen
+    val repositoryMataKuliah: RepositoryMataKuliah
+}
+
+
+class ContainerApp(private val context: Context) : InterfaceContainerApp {
+
+    override val repositoryDosen: RepositoryDosen by lazy{
+        LocalRepositoryDosen(DatabaseAll.getDatabase(context).DosenDao())
+    }
+
+    override val repositoryMataKuliah: RepositoryMataKuliah by lazy {
+        LocalRepositoryMataKuliah(DatabaseAll.getDatabase(context).MataKuliahDao())
+    }
+}
