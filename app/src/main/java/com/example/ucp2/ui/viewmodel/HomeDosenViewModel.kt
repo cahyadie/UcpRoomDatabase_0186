d2c5c960c92e4c93bba4fd38194f6687a0b45b38
cahyadie <cahyadiilham12@gmail.com>
2024-12-21 18:01:05 +0700

    mengubah dari homeviewmodel menjadi HomeDosenViewModel.kt

--- app/src/main/java/com/example/ucp2/ui/viewmodel/HomeDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/HomeDosenViewModel.kt
@@ -5,29 +5,24 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.praktikum9.repository.RepositoryDosen
 import com.example.ucp2.Data.entity.Dosen
-import com.example.ucp2.Data.entity.MataKuliah
-import com.example.ucp2.repository.RepositoryMataKuliah
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 
 class HomeViewModel(
     private val repositoryDosen: RepositoryDosen,
-    private val repositoryMataKuliah: RepositoryMataKuliah
 ) : ViewModel(){
 
-    val HomeUiState: StateFlow<HomeUiState> = combine(repositoryDosen.getAllDosen().filterNotNull(),
-        repositoryMataKuliah.getAllMataKuliah().filterNotNull())
-
-         {  dosenlist, matakuliahlist ->
+    val homeUiState: StateFlow<HomeUiState> = repositoryDosen.getAllDosen()
+        .filterNotNull()
+        .map {
             HomeUiState(
-                listDosen = dosenlist.toList(),
-                listMataKuliah = matakuliahlist.toList(),
+                listDosen = it.toList(),
                 isLoading = false
             )
         }
@@ -51,11 +46,11 @@ class HomeViewModel(
                 isLoading = true,
             )
         )
+
 }
 
 data class HomeUiState(
     val listDosen: List<Dosen> = listOf(),
-    val listMataKuliah: List<MataKuliah> = listOf(),
     val isLoading: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String = ""
